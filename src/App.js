5cc74518591d3c137e7649ca2ca2dee5fca50390
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-11 21:44:26 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const value1 = Math.floor(Math.random() * 100);
+const value2 = Math.floor(Math.random() * 100);
+const value3 = Math.floor(Math.random() * 100);
+const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numQuestions: 0,
+      numCorrect: 0
+    };
+  }
+  handleAnswer(guessIsCorrect) {
+    if (guessIsCorrect)
+      this.setState(currentState => ({
+        numCorrect: currentState.numCorrect + 1
+      }));
+    this.setState(currentState => ({
+      numQuestions: currentState.numQuestions + 1
+    }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <div className="equation">
+            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+          </div>
+          <button>True</button>
+          <button>False</button>
+          <p className="text">
+            Your Score: {numCorrect}/{numQuestions}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
